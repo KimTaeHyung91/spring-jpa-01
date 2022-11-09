2b7b53067ed3377e529ec4a090fc50afbde922dc
james-zent <james@zent.kr>
2022-11-09 19:18:26 +0900

    init commit

---
+++ src/main/java/com/example/springjpach01/SpringJpaCh01Application.java
@@ -0,0 +1,13 @@
+package com.example.springjpach01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaCh01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaCh01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjpach01/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.springjpach01;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUserName())
+                .isEqualTo(member.getUserName());
+    }
+}
